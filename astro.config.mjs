fbab0127187877f5f64dd35a2b28800952e39966
Eli Nelson <165221493+Papaya-Voldemort@users.noreply.github.com>
2026-04-19 16:29:46 -0600

    Replace Node adapter with Vercel adapter in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [react()],
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
